1c347da81b6b47c1f9be719f15c26ebf0534a997
darkhan.jumabekov@gmail.com <darkhan.jumabekov@gmail.com>
2023-07-12 22:26:29 +0600

    Setting up the basic principles of the project

---
+++ HotelManagemnetSystemHW/Controllers/RoomsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagemnetSystemHW.Controllers
+{
+  public class RoomsController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
--- HotelManagemnetSystemHW/Program.cs
+++ HotelManagemnetSystemHW/Program.cs
@@ -1,6 +1,39 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// 1.1 Добавить сервис AddControllersWithViews ()
+builder.Services.AddControllersWithViews();
+
+// 1.4 Добавить возможость использование рейзер движка
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// 1.2 Добавить Страницу ошибку по дефолту
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+// 1.4 Добавить https переходы
+app.UseHttpsRedirection();
+
+// 1.4 Добавить использования статических файлов
+app.UseStaticFiles();
+
+// 1.4 Добавить использования маршрутизации
+app.UseRouting();
+
+// 1.4 Добавить использование аутентификации и авторизации
+app.UseAuthentication();
+app.UseAuthorization();
+
+// 1.3 Настроить MapControllerRoute()
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// 1.4 Добавить возможость использование рейзер движка
+app.MapRazorPages();
 
 app.Run();
