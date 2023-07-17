0d6644c8ec5fe7ff40250ae3c65752133ccfb9d7
DKBdg <strong.every@list.ru>
2023-07-17 19:43:43 +0600

    hw2

--- hw_1/Controllers/AdminController.cs
+++ hw_1/Controllers/AdminController.cs
@@ -7,6 +7,24 @@ namespace hw_1.Controllers
         public IActionResult Index()
         {
             return View();
+
+        }
+        public IActionResult HttpHeaders()
+        {
+            Dictionary<string, string> httphdrs = new Dictionary<string, string>();
+            foreach (var header in Request.Headers)
+            {
+                httphdrs.Add(header.Key, header.Value);
+            }
+            return View(httphdrs);
+        }
+
+        public IActionResult Privacy()
+        {
+            List<string> messages = new List<string>();
+            messages.Add("Everything is safe");
+            return View(messages);
+
         }
     }
 }
